8748c7d334bd5ddc6b7260cfade835edaf7f5f0a
GrantWishes <grant-gertsen@uiowa.edu>
2016-09-09 15:15:09 -0500

    Starting to implement the actual multiplication

--- problem3.c
+++ problem3.c
@@ -5,11 +5,40 @@
 #define MAX_NUM_LENGTH 10000
 
 void multiply_and_print(char* num1, char* num2) {
-	// Implement this function.
+	int index1 = -1;
+	int index2 = -1;
+
+	for(int i = 0; num1[i] != '\0'; i++) {
+		index1++;
+	}
+	for(int i = 0; num2[i] != '\0'; i++) {
+		index2++;
+	}
+	
+	int *product;
+
+	product = calloc(MAX_NUM_LENGTH*MAX_NUM_LENGTH,sizeof(int));
+
+//	printf("Index 1 is %d and Index 2 is %d\n", index1, index2);
+
+	int temp;
+	int prodIndex = 0;
+	int carry = 0;
+
+
+	for(index1; index1 >= 0; index1--) { 
+		for(index2; index2 >= 0; index2--) { 
+			temp = (int)(num1[index1]-'0') * (int)(num2[index2]-'0' + carry);
+			if(temp >= 10) {
+				carry = temp / 10
+			} 
+		}
+	}
 }
 
 int main (int argc , char * argv []) {
-	FILE *file = fopen("problem3.input","r");
+	FILE *file = fopen("fake.input","r");
+//	FILE *file = fopen("problem3.input","r");
 	if (file == NULL) {
 		printf("Input file not found.\n");
 		return 1;
